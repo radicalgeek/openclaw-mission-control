82bdf3b487cc93ea79815f37bfda5ef6a68d9e5e
Mark Jones <mark@radicalgeek.co.uk>
2026-04-03 20:27:22 +0100

    feat(brand): add Product Foundry text label next to logo in header

--- frontend/src/components/atoms/BrandMark.tsx
+++ frontend/src/components/atoms/BrandMark.tsx
@@ -1,10 +1,15 @@
 export function BrandMark() {
   return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img
-      src="/axiacraft-logo.png"
-      alt="AxiaCraft ProductFoundry"
-      className="h-14 w-auto object-contain"
-    />
+    <div className="flex items-center gap-2.5">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/axiacraft-logo.png"
+        alt="AxiaCraft ProductFoundry"
+        className="h-14 w-auto object-contain"
+      />
+      <span className="text-sm font-semibold leading-tight tracking-tight text-[color:var(--text)]">
+        Product<br />Foundry
+      </span>
+    </div>
   );
 }
